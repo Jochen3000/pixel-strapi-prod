f0cbed1286fd1ce8f7a72a8e644eb723d97fbde4
jochen ade <jochen.ade@hotmail.com>
2023-01-03 14:02:56 +0100

    Project Screen working

---
+++ website-nuxt/components/ProjectsDescription.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="projectdescription">
+    <div class="project-intro">
+      <div class="teaserheadline">
+        <h3 class="h3-teaser">
+          My project portfolio includes websites and applications for larger
+          companies, start-ups and non-profit organizations.
+        </h3>
+      </div>
+      <p>
+        Depending on the task, different tools and technologies are used. Here
+        is a selection.
+      </p>
+    </div>
+    <div class="project-1">
+      <div class="teaserheadline">
+        <h3 class="h3-teaser">podojo.com</h3>
+      </div>
+      <p class="paragraph-2">
+        Website for product coaching companies. Product innovation, less risky,
+        faster and with more fun.
+      </p>
+      <div class="project-tags">
+        <div class="tag">
+          <img
+            src="images/tag-icon.svg"
+            loading="lazy"
+            alt=""
+            class="skill-icon"
+          />
+          <div class="text-block-5">ux</div>
+        </div>
+        <div class="tag">
+          <img
+            src="images/tag-icon.svg"
+            loading="lazy"
+            alt=""
+            class="skill-icon"
+          />
+          <div class="text-block-5">figma</div>
+        </div>
+        <div class="tag">
+          <img
+            src="images/tag-icon.svg"
+            loading="lazy"
+            alt=""
+            class="skill-icon"
+          />
+          <div class="text-block-5">css</div>
+        </div>
+        <div class="tag">
+          <img
+            src="images/tag-icon.svg"
+            loading="lazy"
+            alt=""
+            class="skill-icon"
+          />
+          <div class="text-block-5">js</div>
+        </div>
+      </div>
+      <a href="https://podojo.com" class="view-project w-inline-block"
+        ><img
+          src="images/link-chevron.svg"
+          loading="lazy"
+          alt=""
+          class="link-chevron"
+        />
+        <div class="linktext">podojo website</div>
+      </a>
+    </div>
+    <div class="project-2">
+      <div class="teaserheadline">
+        <h3 class="h3-teaser">SaaS Dashboard</h3>
+      </div>
+      <p>
+        Successful Bubble.io template (1000+ installs). Single page application
+        with frontend modules for creating dashboards or CRM functions.
+      </p>
+      <div class="project-tags">
+        <div class="tag">
+          <img
+            src="images/tag-icon.svg"
+            loading="lazy"
+            alt=""
+            class="skill-icon"
+          />
+          <div class="text-block-5">bubble.io</div>
+        </div>
+        <div class="tag">
+          <img
+            src="images/tag-icon.svg"
+            loading="lazy"
+            alt=""
+            class="skill-icon"
+          />
+          <div class="text-block-5">css</div>
+        </div>
+      </div>
+      <a
+        href="https://bubble.io/template/responsive-webapp-flexbox-1651496273502x125461575471464450"
+        class="view-project w-inline-block"
+        ><img
+          src="images/link-chevron.svg"
+          loading="lazy"
+          alt=""
+          class="link-chevron"
+        />
+        <div class="linktext">SaaS dashboard template</div>
+      </a>
+    </div>
+    <div class="project-3">
+      <div class="teaserheadline">
+        <h3 class="h3-teaser">Apress</h3>
+      </div>
+      <p>
+        E-commerce website for Apress in NY. Implemented during my time as a
+        product manager at SpringerNature.
+      </p>
+      <div class="project-tags">
+        <div class="tag">
+          <img
+            src="images/tag-icon.svg"
+            loading="lazy"
+            alt=""
+            class="skill-icon"
+          />
+          <div class="text-block-5">po</div>
+        </div>
+        <div class="tag">
+          <img
+            src="images/tag-icon.svg"
+            loading="lazy"
+            alt=""
+            class="skill-icon"
+          />
+          <div class="text-block-5">ux</div>
+        </div>
+      </div>
+      <a href="https://apress.com" class="view-project w-inline-block"
+        ><img
+          src="images/link-chevron.svg"
+          loading="lazy"
+          alt=""
+          class="link-chevron"
+        />
+        <div class="linktext">Apress website</div>
+      </a>
+    </div>
+  </div>
+</template>
--- website-nuxt/pages/index.vue
+++ website-nuxt/pages/index.vue
@@ -3,7 +3,7 @@
   <div class="page-container w-container">
     <div class="content wf-section">
       <div class="containerinner content">
-        <HomeBlog />
+        <!-- <HomeBlog /> -->
         <div id="projects" class="projectsblock">
           <HomeProjects />
         </div>
