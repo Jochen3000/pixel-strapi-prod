4d83b9ed24ba80fe9a8fd4e9e312dcb73a47f5ee
jochen ade <jochen.ade@hotmail.com>
2023-01-03 14:02:48 +0100

    Project Screen working

--- website-nuxt/components/HomeProjects.vue
+++ website-nuxt/components/HomeProjects.vue
@@ -3,193 +3,73 @@
   <h2 class="center">Projekte</h2>
   <div class="projectsblockcontainer">
     <div class="projectsdisplay">
-      <div class="browser-bar">
+      <div class="browser-bar" v-on:click="showOverview">
         <img src="images/light.svg" loading="lazy" alt="" class="safari" />
       </div>
       <div class="screencontainer">
         <div class="screenstartpage">
-          <h4 class="startpageheading">Favourites</h4>
+          <!-- Projects Display -->
           <div class="favouitesitesblock">
-            <div class="favouriteproject">
-              <img
-                src="images/podojo.png"
-                loading="lazy"
-                alt="podojo thumbnail"
-              />
-              <div class="text-block-3">podojo.com</div>
-            </div>
-            <div class="favouriteproject">
-              <img
-                src="images/saas2.png"
-                loading="lazy"
-                alt="SaaS Template thumbnail"
-              />
-              <div class="text-block-4">SaaS Template</div>
-            </div>
-            <div class="favouriteproject">
-              <img
-                src="images/apress-small.png"
-                loading="lazy"
-                alt="Apress thumbnail"
-              />
-              <div class="text-block-4">Apress</div>
+            <div
+              v-for="(project, index) in projects.data"
+              v-on:click="showDetails(index)"
+              class="favouriteproject"
+            >
+              <!-- Overview -->
+              <div v-if="showProjectOverview === true">
+                <img
+                  :src="
+                    config.public.baseUrl +
+                    project.attributes.preview_image.data.attributes.formats
+                      .medium.url
+                  "
+                  loading="lazy"
+                  sizes="(max-width: 479px) 39vw, 160px"
+                  alt=""
+                  className="image"
+                />
+                <div class="text-block-3">{{ project.attributes.title }}</div>
+              </div>
+              <!-- Details -->
+              <div class="project-container" v-if="index === selectedItemIndex">
+                <img
+                  :src="
+                    config.public.baseUrl +
+                    project.attributes.preview_image.data.attributes.formats
+                      .medium.url
+                  "
+                  loading="lazy"
+                />
+              </div>
             </div>
           </div>
         </div>
-        <div class="project-container"></div>
         <div class="loading-wrapper">
           <div class="loading-bar"></div>
         </div>
       </div>
     </div>
-    <div class="projectdescription">
-      <div class="project-intro">
-        <div class="teaserheadline">
-          <h3 class="h3-teaser">
-            My project portfolio includes websites and applications for larger
-            companies, start-ups and non-profit organizations.
-          </h3>
-        </div>
-        <p>
-          Depending on the task, different tools and technologies are used. Here
-          is a selection.
-        </p>
-      </div>
-      <div class="project-1">
-        <div class="teaserheadline">
-          <h3 class="h3-teaser">podojo.com</h3>
-        </div>
-        <p class="paragraph-2">
-          Website for product coaching companies. Product innovation, less
-          risky, faster and with more fun.
-        </p>
-        <div class="project-tags">
-          <div class="tag">
-            <img
-              src="images/tag-icon.svg"
-              loading="lazy"
-              alt=""
-              class="skill-icon"
-            />
-            <div class="text-block-5">ux</div>
-          </div>
-          <div class="tag">
-            <img
-              src="images/tag-icon.svg"
-              loading="lazy"
-              alt=""
-              class="skill-icon"
-            />
-            <div class="text-block-5">figma</div>
-          </div>
-          <div class="tag">
-            <img
-              src="images/tag-icon.svg"
-              loading="lazy"
-              alt=""
-              class="skill-icon"
-            />
-            <div class="text-block-5">css</div>
-          </div>
-          <div class="tag">
-            <img
-              src="images/tag-icon.svg"
-              loading="lazy"
-              alt=""
-              class="skill-icon"
-            />
-            <div class="text-block-5">js</div>
-          </div>
-        </div>
-        <a href="https://podojo.com" class="view-project w-inline-block"
-          ><img
-            src="images/link-chevron.svg"
-            loading="lazy"
-            alt=""
-            class="link-chevron"
-          />
-          <div class="linktext">podojo website</div>
-        </a>
-      </div>
-      <div class="project-2">
-        <div class="teaserheadline">
-          <h3 class="h3-teaser">SaaS Dashboard</h3>
-        </div>
-        <p>
-          Successful Bubble.io template (1000+ installs). Single page
-          application with frontend modules for creating dashboards or CRM
-          functions.
-        </p>
-        <div class="project-tags">
-          <div class="tag">
-            <img
-              src="images/tag-icon.svg"
-              loading="lazy"
-              alt=""
-              class="skill-icon"
-            />
-            <div class="text-block-5">bubble.io</div>
-          </div>
-          <div class="tag">
-            <img
-              src="images/tag-icon.svg"
-              loading="lazy"
-              alt=""
-              class="skill-icon"
-            />
-            <div class="text-block-5">css</div>
-          </div>
-        </div>
-        <a
-          href="https://bubble.io/template/responsive-webapp-flexbox-1651496273502x125461575471464450"
-          class="view-project w-inline-block"
-          ><img
-            src="images/link-chevron.svg"
-            loading="lazy"
-            alt=""
-            class="link-chevron"
-          />
-          <div class="linktext">SaaS dashboard template</div>
-        </a>
-      </div>
-      <div class="project-3">
-        <div class="teaserheadline">
-          <h3 class="h3-teaser">Apress</h3>
-        </div>
-        <p>
-          E-commerce website for Apress in NY. Implemented during my time as a
-          product manager at SpringerNature.
-        </p>
-        <div class="project-tags">
-          <div class="tag">
-            <img
-              src="images/tag-icon.svg"
-              loading="lazy"
-              alt=""
-              class="skill-icon"
-            />
-            <div class="text-block-5">po</div>
-          </div>
-          <div class="tag">
-            <img
-              src="images/tag-icon.svg"
-              loading="lazy"
-              alt=""
-              class="skill-icon"
-            />
-            <div class="text-block-5">ux</div>
-          </div>
-        </div>
-        <a href="https://apress.com" class="view-project w-inline-block"
-          ><img
-            src="images/link-chevron.svg"
-            loading="lazy"
-            alt=""
-            class="link-chevron"
-          />
-          <div class="linktext">Apress website</div>
-        </a>
-      </div>
-    </div>
+    <ProjectsDescription />
   </div>
 </template>
+
+<script setup>
+const config = useRuntimeConfig();
+const showProjectOverview = ref(true);
+const selectedItemIndex = ref(null);
+
+const { data: projects } = await useFetch(
+  `${config.public.baseUrl}/api/projects?locale=all&populate=*`
+);
+
+const showDetails = (index) => {
+  showProjectOverview.value = false;
+  selectedItemIndex.value = index;
+  console.log(index);
+};
+
+const showOverview = () => {
+  showProjectOverview.value = true;
+  selectedItemIndex.value = null;
+};
+</script>
--- website-nuxt/pages/develop.vue
+++ website-nuxt/pages/develop.vue
@@ -1,11 +1,197 @@
-<script setup lang="ts">
-const markdownString = "# Markdwon-it in Nuxt3";
-</script>
+<script setup>
+/**
+ * projects section
+ */
 
-<template>
-  <h2>Hallo</h2>
-  <section
-    class="prose prose-gray text-sm"
-    v-html="$mdRenderer.render(markdownString)"
-  />
-</template>
+const favouriteProjects = document.querySelectorAll(".favouriteproject");
+const loadingWrapper = document.querySelector(".loading-wrapper");
+const startScreen = document.querySelector(".screenstartpage");
+const screenContent = document.querySelector(".project-container");
+const loadingBar = document.querySelector(".loading-bar");
+const browserBar = document.querySelector(".browser-bar");
+const projectDescriptionIntro = document.querySelector(".project-intro");
+const projectDescription = [
+  document.querySelector(".project-1"),
+  document.querySelector(".project-2"),
+  document.querySelector(".project-3"),
+];
+
+projectDescription[0].classList.add("hide");
+projectDescription[1].classList.add("hide");
+projectDescription[2].classList.add("hide");
+
+browserBar.addEventListener("click", () => {
+  startScreen.classList.remove("hide");
+  projectDescriptionIntro.classList.remove("hide");
+  projectDescription[0].classList.add("hide");
+  projectDescription[1].classList.add("hide");
+  projectDescription[2].classList.add("hide");
+  screenContent.innerHTML = "";
+});
+
+for (let i = 0; i < favouriteProjects.length; i++) {
+  favouriteProjects[i].addEventListener("click", (event) => {
+    loadingWrapper.classList.add("loading");
+    setTimeout(function () {
+      screenContent.insertAdjacentHTML(
+        "afterbegin",
+        `<img src="/images/project-${i}.png">`
+      );
+      startScreen.classList.add("hide");
+      projectDescriptionIntro.classList.add("hide");
+      loadingBar.classList.add("loading-progress");
+      projectDescription[i].classList.remove("hide");
+    }, 250);
+    setTimeout(function () {
+      loadingWrapper.classList.remove("loading");
+      loadingBar.classList.remove("loading-progress");
+    }, 1250);
+  });
+}
+
+/**
+ * whatsapp simulation
+ */
+
+/* display time in top bar */
+setInterval(currentTime, 30000);
+
+function currentTime() {
+  let d = new Date();
+  let hours = d.getHours();
+  let minutes = d.getMinutes();
+  let minutesString = minutes <= 9 ? `0${minutes}` : `${minutes}`;
+  document.querySelector(
+    ".time-display"
+  ).innerHTML = `${hours}:${minutesString}`;
+}
+
+/* select elements and hide */
+const isTyping = document.querySelector(".contact-tap");
+const messagesComputer = document.querySelectorAll(".message-computer");
+const messagesUser = document.querySelectorAll(".message-user");
+const textEntryName = document.querySelector(".text-entry-name");
+const messageSend = document.querySelector(".message-send");
+const textEntryMessage = document.querySelector(".text-entry-message");
+const textEntryContact = document.querySelector(".text-entry-contact");
+const textEntryButton = document.querySelector(".submit-arrow-button");
+const formSubmitButton = document.querySelector(".submit-button");
+const whatsappForm = document.querySelector(".whatsapp-form");
+const chatContainer = document.querySelector(".chat-container");
+const chatContainerInner = document.querySelector(".chat-container-inner");
+
+isTyping.classList.add("hide");
+messagesUser[0].classList.add("hide");
+messagesUser[1].classList.add("hide");
+messagesUser[2].classList.add("hide");
+messagesComputer[1].classList.add("hide");
+messagesComputer[2].classList.add("hide");
+messagesComputer[3].classList.add("hide");
+messagesComputer[4].classList.add("hide");
+textEntryMessage.classList.add("hide");
+textEntryContact.classList.add("hide");
+formSubmitButton.classList.add("hide");
+
+// function to display computer messages
+const displayComputerMessage = (id) => {
+  isTyping.classList.remove("hide");
+  setTimeout(function () {
+    isTyping.classList.add("hide");
+    messagesComputer[id].classList.remove("hide");
+    scrollUp();
+  }, 1000);
+};
+
+// Show first message on scroll visible
+messagesComputer[0].classList.add("hide");
+const appear = messagesComputer[0];
+const cb = function (entries) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      displayComputerMessage(0);
+    }
+  });
+};
+const io = new IntersectionObserver(cb);
+io.observe(appear);
+
+// Show second message on focus textEntryName
+textEntryName.addEventListener("focus", () => {
+  displayComputerMessage(1);
+});
+
+// Press submit button to send user messages
+textEntryButton.addEventListener("click", () => {
+  displayUserMessage();
+});
+
+// Press enter key to send user messages
+whatsappForm.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    displayUserMessage();
+    e.preventDefault();
+  }
+});
+
+// scroll up window if needed
+const scrollUp = () => {
+  if (chatContainerInner.clientHeight > chatContainer.clientHeight) {
+    chatContainer.scrollTop =
+      chatContainerInner.clientHeight - chatContainer.clientHeight;
+  }
+};
+
+// Check which fields are filled and display
+const displayUserMessage = () => {
+  if (textEntryContact.value) {
+    messagesUser[2].firstElementChild.firstElementChild.firstElementChild.innerText =
+      textEntryContact.value;
+    messagesUser[2].classList.remove("hide");
+    textEntryButton.classList.add("hide");
+    displayComputerMessage(4);
+    formSubmitButton.click();
+    scrollUp();
+  } else if (textEntryMessage.value) {
+    messagesUser[1].firstElementChild.firstElementChild.firstElementChild.innerText =
+      textEntryMessage.value;
+    messagesUser[1].classList.remove("hide");
+    textEntryContact.classList.remove("hide");
+    textEntryContact.focus();
+    textEntryMessage.classList.add("hide");
+    displayComputerMessage(3);
+    scrollUp();
+  } else if (textEntryName.value) {
+    messagesUser[0].firstElementChild.firstElementChild.firstElementChild.innerText =
+      textEntryName.value;
+    messagesUser[0].classList.remove("hide");
+    textEntryMessage.classList.remove("hide");
+    textEntryMessage.focus();
+    textEntryName.classList.add("hide");
+    displayComputerMessage(2);
+    scrollUp();
+  }
+};
+
+// netlify form submission
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => {
+      setTimeout(function () {
+        messageSend.classList.add("hide");
+        console.log("form submitted");
+      }, 1000);
+    })
+    .catch((error) => alert(error));
+};
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
+</script>
