894e41615705d2892eaf6db2900d24e45513d0b6
jochen ade <jochen.ade@hotmail.com>
2023-01-10 14:46:49 +0100

    added salt

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', '16IA0zzEUyszIwSt+DoZpQ=='),
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', '6cea2d030a6fe5fbadc2843096e4dd93'),
   },
